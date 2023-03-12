a7525aac64c018d3becbf486ccdf529e30a3368d
narven <narven_olga@hotmail.com>
2023-03-12 08:41:12 +0300

    013

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+//32679 -> 6
+
+void checkThirdNumber(int a)
+{
+if (a < 100)
+{
+
+System.Console.WriteLine("Нет третьей цифры");
+return;
+}
+while (a > 999)
+{
+a /= 10;
+}
+System.Console.WriteLine($"Третья цифра - {a % 10}");
+}
+System.Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+checkThirdNumber(a);
